1d8c34d76a2a0d94ad97377f67f0a0b5d3492b6e
Liao Zhu <liao.zhu.cs@gmail.com>
2023-07-29 09:54:07 -0500

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGear } from '@fortawesome/free-solid-svg-icons';
+import SettingsDropdown from './SettingsDropdown';
 
-function Navbar() {
+function Navbar({ signOut }) {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown((prevShowDropdown) => !prevShowDropdown);
+  };
+
+  const handleClickOutside = (event) => {
+    if (showDropdown && !event.target.closest('.settings-dropdown')) {
+      setShowDropdown(false);
+    }
+  };
+
   return (
-    <div className="flex fixed z-1 top-0 right-0 w-full justify-end items-center p-4">
-        <div className="flex justify-center items-center">
-            <div className="flex justify-center items-center text-3xl transition-transform transform hover:rotate-45 cursor-pointer">
-                <FontAwesomeIcon icon={faGear} />
-            </div>
+    <div
+      className="flex fixed z-1 top-0 right-0 w-full justify-end items-center p-4"
+      onClick={handleClickOutside}
+    >
+      <div className="flex justify-center items-center relative">
+        <div
+          className="flex justify-center items-center text-3xl transition-transform transform hover:rotate-45 cursor-pointer"
+          onClick={toggleDropdown}
+        >
+          <FontAwesomeIcon icon={faGear} />
         </div>
+        {showDropdown && <SettingsDropdown signOut={signOut}/>}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
